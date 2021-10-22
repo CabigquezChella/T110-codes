b81105a1c316db721eaf626e3898c0629fcc7452
CabigquezChella <89560054+CabigquezChella@users.noreply.github.com>
2021-10-22 16:25:20 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,29 @@
 package com.chelecious;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("A spinning tire makes 3000 revolutions per minutes. " +
-                "Calculate How many degrees it rotate in one second.\n");
-         int numRev=3000,degree=360,seconds=60,sum=0;
-         sum+=numRev*degree/seconds;
-        System.out.println("The motor rotates " +sum+ " degrees in one second");
+      Scanner input=new Scanner(System.in);
+        System.out.print("Input A Positive Integers: ");
+        int number=input.nextInt();
+        theData(number);
+
+
+
+
+        }
+    public static void theData(int number){
+
+        while (number>1){
+            if (number%2==0) number/=2;
+            else number=3*number+1;
+            System.out.println("The Current Given Value is " +number+(number==1?"":" "));
+
+        }
+
+        System.out.println( );
+    }
 
     }
-}
+
